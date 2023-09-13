7959dff44abc4e10ce541748935a8b406e932dc8
Luiz Bender <LuizBender@outlook.com.br>
2023-09-13 16:24:20 -0300

    test

---
+++ VShop.IdentityServer/Program.cs
@@ -0,0 +1,82 @@
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VShop.IdentityServer.Configuration;
+using VShop.IdentityServer.Data;
+using VShop.IdentityServer.SeedDatabase;
+using VShop.IdentityServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+var host = builder.Configuration["DBHOST"] ?? "localhost";
+var port = builder.Configuration["DBPORT"] ?? "3306";
+var password = builder.Configuration["DBPASSWORD"] ?? "numsey";
+
+string mySqlConnection = $"server={host};userid=root;pwd={password};"
+                         + $"port={port};database=VShopIdentityServer";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                              options.UseMySql(mySqlConnection,
+                              ServerVersion.AutoDetect(mySqlConnection)));
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+     //configura��es dos servi�os do IdentityServer
+    var builderIdentityServer = builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+        options.EmitStaticAudienceClaim = true;
+    }).AddInMemoryIdentityResources(
+                           IdentityConfiguration.IdentityResources)
+                           .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                           .AddInMemoryClients(IdentityConfiguration.Clients)
+                           .AddAspNetIdentity<ApplicationUser>();
+
+    builderIdentityServer.AddDeveloperSigningCredential();
+
+builder.Services.AddScoped<IDatabaseSeedInitializer, DatabaseIdentityServerInitializer>();
+builder.Services.AddScoped<IProfileService, ProfileAppService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+SeedDatabaseIdentityServer(app);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void SeedDatabaseIdentityServer(IApplicationBuilder app)
+{
+    using (var serviceScope = app.ApplicationServices.CreateScope())
+    {
+        var initRolesUsers = serviceScope.ServiceProvider
+                               .GetService<IDatabaseSeedInitializer>();
+
+        initRolesUsers.InitializeSeedRoles();
+        initRolesUsers.InitializeSeedUsers();
+    }
+}
